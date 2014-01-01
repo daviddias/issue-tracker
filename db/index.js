e8b4c871c11fa1ff32f261d09621c17a69c0d1c7
David Dias <daviddias.p@gmail.com>
2014-01-01 15:01:46 +0000

    now with new sugar on the adapter creation, thanks to mde!

--- db/index.js
+++ db/index.js
@@ -3,11 +3,13 @@ var path = require('path');
 
 /// level adapter hack
 
-var Adapter = require(path.join(path.dirname(require.resolve('model')), 'adapters', 'level')).Adapter
+// var Adapter = require(path.join(path.dirname(require.resolve('model')), 'adapters', 'level')).Adapter
 var config  = require('./db.json');
 
+
 exports = module.exports; 
-exports.adapter = new Adapter(config.level);
+// exports.adapter = new Adapter(config.level);
+exports.adapter = model.createAdapter('level', config.level)
 
 /// require the models
 
