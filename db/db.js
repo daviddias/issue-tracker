529d3f8becea4fa21f59c6b8f1c7af19691e80fa
David Dias <daviddias.p@gmail.com>
2014-01-22 13:06:42 +0000

    added dev mode

--- db/db.js
+++ db/db.js
@@ -1,8 +1,14 @@
 var userHome = require('osenv').home();
 
-module.exports =
-{
+var dbConfig = {
   'level': {
     'db': userHome + '/.trckr-db/'
   }
 };
+
+if (process.env.NODE_ENV === 'dev'){
+  console.log('RUNNING ON DEV MODE');
+  dbConfig.level.db = userHome + '/.trckr-db-dev/';
+}
+
+module.exports = dbConfig;
