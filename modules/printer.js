cceb3487d8310e1665b607b5094e7f95b0ed9172
David Dias <daviddias.p@gmail.com>
2014-02-17 07:02:21 +0000

    added labels, milestones and assignines to the print

--- modules/printer.js
+++ modules/printer.js
@@ -13,11 +13,28 @@ exports.issue = function(issue) {
 exports.issues = function (issues) {
   var filtered = [];
   for (var i=0;i<issues.length;i++){
+    var labels = [];
+    var assignee = issues[i].assignee ? issues[i].assignee.login : '';
+    var milestone = issues[i].milestone ? issues[i].milestone.title : '';
+    var last_review =
+    issues[i].trckrLastReview ? issues[i].trckrLastReview : 'not reviewed';
+
+    if (issues[i].labels.length>0){
+      for(var j=0;j<issues[i].labels.length;j++){
+        labels.push(issues[i].labels[j].name);
+      }
+    } else {
+      labels.push('');
+    }
+
     filtered.push({
       number: issues[i].number,
-      title: issues[i].title,
+      title: issues[i].title.substring(0, 42) + '...',
       url: issues[i].htmlUrl,
-      last_review: issues[i].trckrLastReview
+      last_review: last_review,
+      labels: labels.toString(),
+      assignee: assignee,
+      milestone: milestone
     });
   }
   pr.pt(filtered);
